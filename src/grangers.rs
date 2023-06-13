2ca92df0b11d5af7ad02aa98fa9b72894f56679d
Rob Patro <rob@newton>
2023-06-13 11:55:53 -0400

    add info to Cargo.toml

--- src/grangers.rs
+++ src/grangers.rs
@@ -1,5 +1,4 @@
+pub mod grangers_info;
 pub mod grangers_utils;
 pub mod options;
 pub mod reader;
-pub mod grangers_info;
-pub use grangers_info::Grangers as Grangers;
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod grangers;
+pub use grangers::grangers_info;
 pub use grangers::grangers_utils;
 pub use grangers::options;
 pub use grangers::reader;
-pub use grangers::grangers_info;
-pub use grangers_info::Grangers as Grangers;
+pub use grangers_info::Grangers;
